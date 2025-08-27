ab4dcf4d3bd7fb1dd4dd8c3b20298965db4f107d
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-27 10:21:26 -0500

    initial

---
+++ src/components/Kennel.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { LocationList } from "./location/Location"
+
+export const Kennel = () => {
+  return (
+    <>
+      <h2>Nashville Kennels</h2>
+      <LocationList />
+    </>
+  )
+}
---
+++ src/components/location/Location.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const LocationList = () => {
+  return (
+    <article>
+      <h2>List of Locations</h2>
+    </article>
+  )
+}
