c15a1b9ddf26c0ecbfa6bbfcd415c99450cd1679
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-28 15:36:51 -0500

    learn

--- src/components/Kennel.jsx
+++ src/components/Kennel.jsx
@@ -1,10 +1,24 @@
 import React from "react"
-
+import { Login } from "./auth/Login"
 import { Route, Routes } from "react-router-dom"
-
+import { Authorized } from "../views/Authorized"
+import { Register } from "./auth/Register"
 import { ApplicationViews } from "../views/ApplicationViews"
-import { NavBar } from "./nav/NavBar"
 
 export const Kennel = () => {
-  return <ApplicationViews />
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route
+        path="*"
+        element={
+          <Authorized>
+            <ApplicationViews />
+          </Authorized>
+        }
+      />
+    </Routes>
+  )
 }
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,3 +1,120 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { AnimalList } from "../components/animal/AnimalList"
+import { AnimalProvider } from "../components/animal/AnimalProvider"
+import { CustomersProvider } from "../components/customers/CustomerProvider"
+import { CustomersList } from "../components/customers/CustomersList"
+import { EmployeeList } from "../components/employees/EmployeeList"
+import { EmployeeProvider } from "../components/employees/EmployeeProvider"
+import { LocationList } from "../components/location/Location"
+import { LocationProvider } from "../components/location/LocationProvider"
+import { NavBar } from "../components/nav/NavBar"
+import { Welcome } from "../components/welcome/Welcome"
+import { AddAnimal } from "../components/forms/AddAnimal"
+import { HireEmployee } from "../components/forms/HireEmployee"
+import { NewLocation } from "../components/forms/NewLocation"
+
 export const ApplicationViews = () => {
-  return <></>
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<Welcome />} />
+
+        <Route path="locations">
+          <Route
+            index
+            element={
+              <>
+                <LocationProvider>
+                  <LocationList />
+                </LocationProvider>
+              </>
+            }
+          />
+          <Route
+            path="new"
+            element={
+              <>
+                <LocationProvider>
+                  <NewLocation />
+                </LocationProvider>
+              </>
+            }
+          />
+        </Route>
+
+        <Route path="animals">
+          <Route
+            index
+            element={
+              <>
+                <AnimalProvider>
+                  <AnimalList />
+                </AnimalProvider>
+              </>
+            }
+          />
+          <Route
+            path="add"
+            element={
+              <>
+                <>
+                  <LocationProvider>
+                    <CustomersProvider>
+                      <AnimalProvider>
+                        <AddAnimal />
+                      </AnimalProvider>
+                    </CustomersProvider>
+                  </LocationProvider>
+                </>
+              </>
+            }
+          />
+        </Route>
+
+        <Route path="employees">
+          <Route
+            index
+            element={
+              <>
+                <EmployeeProvider>
+                  <EmployeeList />
+                </EmployeeProvider>
+              </>
+            }
+          />
+          <Route
+            path="hire"
+            element={
+              <>
+                <LocationProvider>
+                  <EmployeeProvider>
+                    <HireEmployee />
+                  </EmployeeProvider>
+                </LocationProvider>
+              </>
+            }
+          />
+        </Route>
+
+        <Route
+          path="customers"
+          element={
+            <>
+              <CustomersProvider>
+                <CustomersList />
+              </CustomersProvider>
+            </>
+          }
+        />
+      </Route>
+    </Routes>
+  )
 }
